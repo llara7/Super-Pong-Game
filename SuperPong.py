e568f31c458917e4053496adcc1275597c1a920d
Laura <lllaralaura7@gmail.com>
2019-12-18 23:29:41 -0600

    Update SuperPong.py

--- SuperPong.py
+++ SuperPong.py
@@ -1,9 +1,31 @@
 import turtle
-import winsound
+
 
 wn = turtle.Screen()
 wn.title("Super Pong")
-wn.bgcolor("black")
+wn.bgcolor("blue")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
